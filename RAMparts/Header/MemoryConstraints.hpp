d5e26dc2f4961a77bfa33453dc1e1ba0437143e0
doceallaigh <dylan.oceallaigh@gmail.com>
2017-08-18 21:32:59 -0400

    Added key logic to MemoryConstraints
    Added logic to make MemoryConstraints keyable (This should be separated out into a wrapper class in the future).

--- RAMparts/Header/MemoryConstraints.hpp
+++ RAMparts/Header/MemoryConstraints.hpp
@@ -19,7 +19,28 @@ typedef struct MemoryConstraints
 {
 public:
     size_t MinimumSize = 1U;
-    size_t MaximumSize = std::numeric_limits<size_t>::max();
     size_t MinimumAlignment = alignof(std::max_align_t);
+    size_t MaximumSize = std::numeric_limits<size_t>::max();
     size_t MaximumAlignment = alignof(std::max_align_t);
+
+    bool operator ==(const MemoryConstraints& other) const
+    {
+        return this->MinimumSize == other.MinimumSize &&
+            this->MinimumAlignment == other.MinimumAlignment &&
+            this->MaximumSize == other.MaximumSize &&
+            this->MaximumAlignment == other.MaximumAlignment;
+    }
 } MemoryConstraints;
+
+namespace std
+{
+    template<>
+    struct hash<const MemoryConstraints>
+    {
+        std::size_t operator()(const MemoryConstraints& constraints) const
+        {
+            // TODO Implement this
+            return 0;
+        }
+    };
+}
